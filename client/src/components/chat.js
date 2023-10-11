d70c933d80acfbf75e4d7b1ecb9834c3c2c5865a
Guy Domino <guywayne.domino7@gmail.com>
2023-10-11 18:35:27 -0500

    Added messaging page and css for chat. Routed it to main.jsx

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import io from 'socket.io-client';  
 
@@ -47,6 +47,10 @@ function Chat({ targetLanguage }) {
       <p>Translated Message: {translatedMessage}</p>
     </div>
   );
+}
+
+Chat.propTypes = {
+  
 }
 
 export default Chat;  
