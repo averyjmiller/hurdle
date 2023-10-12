9d4d4c1ca7f9efe90b5acb2a0ae8ceb30a719999
Mona Lisa <krmproductions19@gmail.com>
2023-10-12 19:34:34 -0400

    Added mutations in client

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,27 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./login.css";
-import React, { useState } from 'react';
-import axios from 'axios';  
-import customButton from 'hurdle/app.css';  
+import React, { useState } from "react";
+import axios from "axios";
+// import customButton from 'hurdle/app.css';
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post('/api/login', {
+      const response = await axios.post("/api/login", {
         username,
         password,
       });
 
       if (response.data.success) {
-        localStorage.setItem('preferredLanguage', response.data.preferredLanguage);
+        localStorage.setItem(
+          "preferredLanguage",
+          response.data.preferredLanguage
+        );
       } else {
-        console.error('Login failed:', response.data.error);
+        console.error("Login failed:", response.data.error);
       }
     } catch (error) {
-      console.error('Login error:', error);
+      console.error("Login error:", error);
     }
   };
 
@@ -32,13 +35,13 @@ function LoginPage() {
         type="text"
         placeholder="Username"
         value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        onChange={e => setUsername(e.target.value)}
       />
       <input
         type="password"
         placeholder="Password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={e => setPassword(e.target.value)}
       />
       <button onClick={handleLogin}>Sign In</button>
     </div>
--- server/models/Profile.js
+++ server/models/Profile.js
@@ -37,6 +37,19 @@ const profileSchema = new Schema({
   },
 });
 
+profileSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 const Profile = model("Profile", profileSchema);
 
 module.exports = Profile;
