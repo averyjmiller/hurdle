f891fc4821c6e383f4dde89a40870b086033566a
Avery Miller <averymiller2060@gmail.com>
2023-10-11 20:50:47 -0400

    implemented queries and mutations for typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,32 @@
 const typeDefs = `
+  type Profile {
+    _id: ID
+    name: String
+    username: String
+    email: String
+    password: String
+    language: String
+  }
 
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+  }
+
+  type Mutation {
+    addProfile(name: String!, username: String!, email: String!, password: String!, language: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    updateLanguage(profileId: ID!, newLanguage: String!): Profile
+    updatePassword(profileId: ID!, newPassword: String!): Profile
+    updateEmail(profileId: ID!, newEmail: String!): Profile
+    removeProfile(profileId: ID!): Profile
+  }
 `;
 
 module.exports = typeDefs;
