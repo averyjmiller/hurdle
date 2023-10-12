5858b1b5992a4b972b1468ae63b6ade3d8b3a83a
Guy Domino <guywayne.domino7@gmail.com>
2023-10-11 19:50:01 -0500

    Fixed home page button route

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,14 +1,12 @@
 import { Link } from 'react-router-dom';
 import CustomButton from '../components/customButton';
+import "./home.css";
 const Home = () => {
   return ( 
     <div>
       <h1>Welcome to Hurdle</h1>
       <p>Start messaging with people around the world</p>
-      <CustomButton buttonText='Sign In'/>
-      <CustomButton buttonText='Sign Up'/>
-      {/* "Start Messaging" button */}
-      <Link to="/messaging">
+      <Link to="/login">
       <CustomButton buttonText='Start Messaging'/>
       </Link>
     </div>
