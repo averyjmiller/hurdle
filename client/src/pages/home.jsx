c7b2d226f4d1b2133a7c9d61a74ffb6e789f47ca
Guy Domino <guywayne.domino7@gmail.com>
2023-10-17 17:18:26 -0500

    Added homepage logic to allow signed in users to message

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -3,25 +3,25 @@ import { useEffect } from 'react';
 import CustomButton from '../components/customButton';
 import "./home.css";
 
-const Home = () => {
-  const navigate = useNavigate();
+const Home = () => { 
+  const navigate = useNavigate(); 
 
-  useEffect(() => {
-    // Redirect if the user is already logged in
-    if (localStorage.getItem('token')) {
-      navigate('/messaging');
+  useEffect(() => { 
+    // Redirect if the user is already logged in 
+    if (localStorage.getItem('token')) { 
+      navigate('/messaging'); 
     }
-  }, [navigate]);
+  }, [navigate]);  
 
-  return ( 
-    <div>
-      <h1>Welcome to Hurdle</h1>
-      <p>Start messaging with people around the world</p>
-      <Link to="/login">
-        <CustomButton buttonText='Start Messaging'/>
-      </Link>
-    </div>
-  );
-}
+  return (  
+    <div> 
+      <h1>Welcome to Hurdle</h1> 
+      <p>Start messaging with people around the world</p> 
+      <Link to="/login"> 
+        <CustomButton buttonText='Start Messaging'/> 
+      </Link> 
+    </div> 
+  ); 
+} 
  
-export default Home;
+export default Home; 
