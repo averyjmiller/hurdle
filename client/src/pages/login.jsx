1d4817c2179689e54b50be72db26364bdabd7dc2
Avery Miller <averymiller2060@gmail.com>
2023-10-16 20:56:08 -0400

    implemented logic for login and signup which utilizes jwt

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,51 +1,90 @@
-import { Link } from "react-router-dom";
-import "./login.css";
-import React, { useState } from 'react';
-import axios from 'axios';  
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
+import Auth from '../utils/auth';
 
-function LoginPage() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+import './login.css';
 
-  const handleLogin = async () => {
+const Login = (props) => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
     try {
-      const response = await axios.post("/api/login", {
-        username,
-        password,
+      const { data } = await login({
+        variables: { ...formState },
       });
 
-      if (response.data.success) {
-        localStorage.setItem(
-          "preferredLanguage",
-          response.data.preferredLanguage
-        );
-      } else {
-        console.error("Login failed:", response.data.error);
-      }
-    } catch (error) {
-      console.error("Login error:", error);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
   };
 
   return (
     <div>
-      <h1>Signin Page</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Sign In</button>
+      <h1>Login!</h1>
+        {data ? (
+          <p>
+            Success! You may now head{' '}
+            <Link to="/">back to the homepage.</Link>
+          </p>
+        ) : (
+          <form onSubmit={handleFormSubmit}>
+            <input
+              type="text"
+              placeholder="Email"            
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+            <input
+              placeholder="Password"
+              name="password"
+              type="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+            <button
+              style={{ cursor: 'pointer' }}
+              type="submit"
+            >
+              Sign In
+            </button>
+          </form>
+        )}
+
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">
+            {error.message}
+          </div>
+        )}
+      <Link to="/signup">
+      <p>Create an account</p>
+      </Link>
     </div>
   );
 }
 
-export default LoginPage;
+export default Login;
